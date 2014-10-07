1b655e363c7c881c3836d38c47751df958d2ad4f
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2014-10-07 14:08:06 +0400

    changing layot

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+"use scrict"
+$(function ($) {
+
+  var ht = $(window).height();
+
+  console.log('\'Allo \'Allo!');
+
+  $('.skill-bg').each(function () {
+    $(this).waypoint(function () {
+      var bar = $(this).find('div');
+      var prc = $(this).data('prc');
+      var label = $(this).find('.skill-bar-percent');
+      console.log(prc);
+      bar.animate({'width':prc + '%'}, 200, 'swing', function () {
+        label.countTo({from: 0, to: prc, speed: 400});
+        label.animate({'left':prc + '%'});
+      });
+    }, {
+      offset: ht - 100,
+      triggerOnce: !0
+    });
+  });
+
+});
